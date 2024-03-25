d670aeb491acd66acfc8a80328d152020ecf2e19
Jeremie Bresson <jeremie.bresson@unblu.com>
2024-03-25 10:18:10 +0100

    Remove "Main.kt"

--- integration-tests/gradle/src/main/resources/multi-module-kotlin-project/web/build.gradle.kts
+++ integration-tests/gradle/src/main/resources/multi-module-kotlin-project/web/build.gradle.kts
@@ -1,6 +1,14 @@
 plugins {
+    id("org.jetbrains.kotlin.plugin.allopen")
     id("io.quarkus")
     kotlin("jvm")
+}
+
+allOpen {
+    annotation("jakarta.ws.rs.Path")
+    annotation("jakarta.enterprise.context.ApplicationScoped")
+    annotation("jakarta.persistence.Entity")
+    annotation("io.quarkus.test.junit.QuarkusTest")
 }
 
 dependencies {
